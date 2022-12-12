c1aa8728f625c7c914c47d007b9a0856cd853376
Jurand76 <jurand@interia.pl>
2022-12-12 23:19:11 +0100

    fix: code changed

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        double a, b;
+        a = b = 0;
+        double formula;
+        formula = (9*a*a - 5*b + 2 - 7)*(a+b-4*a*b)/2;
     }
 }
